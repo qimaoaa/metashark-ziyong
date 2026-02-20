2fc9c325f902bff7247f0c3e099ee09922d8dec0
qimaoww <qimaoaa@hotmail.com>
2026-02-20 10:06:30 +0800

    fix: move TVDB ExternalId classes directly into ServiceRegistrator to ensure compiler visibility and fix CI

--- Jellyfin.Plugin.MetaShark/ServiceRegistrator.cs
+++ Jellyfin.Plugin.MetaShark/ServiceRegistrator.cs
@@ -3,8 +3,12 @@ namespace Jellyfin.Plugin.MetaShark
     using Jellyfin.Plugin.MetaShark.Api;
     using Jellyfin.Plugin.MetaShark.Providers.ExternalId;
     using MediaBrowser.Controller;
+    using MediaBrowser.Controller.Entities.Movies;
+    using MediaBrowser.Controller.Entities.TV;
     using MediaBrowser.Controller.Plugins;
     using MediaBrowser.Controller.Providers;
+    using MediaBrowser.Model.Entities;
+    using MediaBrowser.Model.Providers;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
 
@@ -28,5 +32,59 @@ namespace Jellyfin.Plugin.MetaShark
             serviceCollection.AddSingleton<IExternalId, TvdbSeasonExternalId>();
             serviceCollection.AddSingleton<IExternalId, TvdbEpisodeExternalId>();
         }
+    }
+
+    public class TvdbSeriesExternalId : IExternalId
+    {
+        public string ProviderName => "TheTVDB";
+        public string Key => "Tvdb";
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Series;
+        public string UrlFormatString => "https://www.thetvdb.com/?tab=series&id={0}";
+        public bool Supports(IHasProviderIds item) => item is Series;
+    }
+
+    public class TvdbSeriesSlugExternalId : IExternalId
+    {
+        public string ProviderName => "TheTVDB Slug";
+        public string Key => "TvdbSlug";
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Series;
+        public string UrlFormatString => "https://www.thetvdb.com/?tab=series&id={0}";
+        public bool Supports(IHasProviderIds item) => item is Series;
+    }
+
+    public class TvdbMovieExternalId : IExternalId
+    {
+        public string ProviderName => "TheTVDB";
+        public string Key => "Tvdb";
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Movie;
+        public string UrlFormatString => "https://www.thetvdb.com/?tab=series&id={0}";
+        public bool Supports(IHasProviderIds item) => item is Movie;
+    }
+
+    public class TvdbMovieSlugExternalId : IExternalId
+    {
+        public string ProviderName => "TheTVDB Slug";
+        public string Key => "TvdbSlug";
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Movie;
+        public string UrlFormatString => "https://www.thetvdb.com/?tab=series&id={0}";
+        public bool Supports(IHasProviderIds item) => item is Movie;
+    }
+
+    public class TvdbSeasonExternalId : IExternalId
+    {
+        public string ProviderName => "TheTVDB Season";
+        public string Key => "Tvdb";
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Season;
+        public string UrlFormatString => "https://www.thetvdb.com/?tab=season&id={0}";
+        public bool Supports(IHasProviderIds item) => item is Season;
+    }
+
+    public class TvdbEpisodeExternalId : IExternalId
+    {
+        public string ProviderName => "TheTVDB Episode";
+        public string Key => "Tvdb";
+        public ExternalIdMediaType? Type => ExternalIdMediaType.Episode;
+        public string UrlFormatString => "https://www.thetvdb.com/?tab=episode&id={0}";
+        public bool Supports(IHasProviderIds item) => item is Episode;
     }
 }
